7560a42767e349ac20fcb0a01659b621f9fc5cfd
jardelnovaes <jardelnovaes@hotmail.com>
2016-08-23 16:54:20 -0300

    creating

--- app/app/components/appCivico/escolas/schoolController.js
+++ app/app/components/appCivico/escolas/schoolController.js
@@ -20,5 +20,44 @@ app.controller("schoolController", ['$scope', 'schoolService', /*'$routeParams',
 		$scope.getSchools();
 	}
 	
+	$scope.getPreviousSchools = function() {
+		if($scope.schoolsPage > 1){
+			$scope.schoolsPage--;
+			$scope.getSchools();
+		}
+	}
+	
 	$scope.getSchools();
 }]);
+
+/*
+	Angular JS to App
+	http://frontinbrazil.com.br/aplicativos-mobile-com-o-angularjs-e-ionic/
+	http://ionicframework.com/getting-started/
+	http://stackoverflow.com/questions/25961013/how-to-convert-an-existing-angularjs-web-app-to-a-cordova-app
+	http://dzfweb.com.br/2014/12/10/desenvolvimento-mobile-com-apache-cordova-no-visual-studio-com-angularjs/
+	
+	
+	
+	---> templates: blank, tabs e sidemenu
+	
+	# npm install -g cordova ionic
+	# ionic start myApp <template [blank|tabs|sidemenu]>
+	
+	# ionic start <appName> <template>
+	# cd <appName>
+	# ionic platform add ios
+	# ionic platform add android
+	
+	# ionic build ios
+	# ionic emulate ios
+	
+	npm install -g cordova ionic
+	ionic start memApp sidemenu
+	cd memApp
+	ionic platform add android
+	ionic build android
+	ionic emulate android
+	
+
+*/
